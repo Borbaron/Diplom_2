0ab99d9b4a8e8cdb72e7061aaac18d4e61f7376a
Anatoly Kalinichenko <borbaronn@yandex.ru>
2026-01-31 01:47:28 +0300

    feat: Добавил content-type JSON в user_api

--- api/order_api.py
+++ api/order_api.py
@@ -7,11 +7,11 @@ class OrderApiClient:
         self.base_url = Url.BASE_URL
 
     @allure.step("Создание заказа")
-    def create_order(self, order_data, auth_token=None):  # Добавляем auth_token
+    def create_order(self, order_data, auth_token=None):
         url = Url.ORDER_CREATE
         headers = {}
         if auth_token: # Если токен передан, добавляем заголовок авторизации
             headers['Authorization'] = f'Bearer {auth_token}'
 
-        response = requests.post(url, json=order_data, headers=headers)  # Используем headers
+        response = requests.post(url, json=order_data, headers=headers)
         return response
--- api/user_api.py
+++ api/user_api.py
@@ -1,6 +1,7 @@
 import requests
 import allure
 from url import Url
+import json
 
 class UserApiClient:
     def __init__(self):
@@ -9,12 +10,11 @@ class UserApiClient:
     @allure.step("Создание пользователя")
     def create_user(self, user_data):
         url = Url.USER_CREATE
-        response = requests.post(url, data=user_data)
+        response = requests.post(url, data=json.dumps(user_data), headers={'Content-Type': 'application/json'})
         return response
-    
+
     @allure.step("Авторизация пользователя")
     def login_user(self, login_data):
         url = Url.USER_LOGIN
-        response = requests.post(url, data=login_data)
+        response = requests.post(url, data=json.dumps(login_data), headers={'Content-Type': 'application/json'})
         return response
-
